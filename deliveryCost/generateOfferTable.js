d4a76f343c43e5e1fcee5abd94ef4ff0325bf2a3
Kali Live user <purujitkulshreshtha8@gmail.com>
2021-09-17 20:19:42 +0000

    Implemented OfferTable Function
    Implemented a function imported and called in entry point of delivery cost calculator. Purpose: Makes changing, adding and removing test data super simple, just need to put it in 'criteria.txt' in the right format without having to interact with the code. The 'generateOfferTable' function takes care of the rest.

--- deliveryCost/generateOfferTable.js
+++ deliveryCost/generateOfferTable.js
@@ -1,8 +1,30 @@
 const fs = require('fs')
 
 const generateOfferTable = () => {
-	fs.readFile("criteria.txt", "utf8", (err, data) => {
+
+	const data = (fs.readFileSync("./deliveryCost/criteria.txt")).toString()
+	const lines = (data.split("\n")).splice(2, data.length)
+	'OFR001 10 0-200 70-200'
+	const offerTable = {}
+	lines.forEach((line) => {
+		const values = line.split(" ")
+		const offerCode = values[0]
+		const rate = parseInt(values[1])/100
+
+		const distances = values[2].split("-")
+		const weights = values[3].split("-")
+
+		const distanceLow = parseInt(distances[0])
+		const distanceHigh = parseInt(distances[1])
+		const weightLow = parseInt(weights[0])
+		const weightHigh = parseInt(weights[1])
+
+		offerTable[offerCode] = {rate, distanceLow, distanceHigh, weightLow, weightHigh}
+		// console.log(offerTable)
+
 	})
+	return offerTable	
+
 }
 
 module.exports = generateOfferTable
--- deliveryCost/index.js
+++ deliveryCost/index.js
@@ -6,32 +6,7 @@ const fs = require('fs')
 
 const findDeliveryCosts = (input) => {
 
-	// generateOfferTable()
-
-	//lookup table to be stored in a json file or text file to easily make changes
-	const offerTable = {
-		OFR001: {
-			rate: 0.1,
-			distanceLow: 0,
-			distanceHigh: 200,
-			weightLow: 70,
-			weightHigh: 200 
-		},
-		OFR002: {
-			rate: 0.07,
-			distanceLow: 50,
-			distanceHigh: 150,
-			weightLow: 100,
-			weightHigh: 250 
-		},
-		OFR003: {
-			rate: 0.05,
-			distanceLow: 50,
-			distanceHigh: 250,
-			weightLow: 10,
-			weightHigh: 150 
-		}
-	}
+	const offerTable = generateOfferTable()
 
 	const costs = [] //output array
 
